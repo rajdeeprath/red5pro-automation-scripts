8daf9fcd4e06dfd14ec5c1c8cffeed8adff4af64
rajdeeprath <rajdeeprath@gmail.com>
2018-01-17 17:02:06 +0530

    AWS AutoScaling Deployer
    + First commit

---
+++ awsdeployer/src/main/java/com/red5pro/awsdeployer/AutoDeployer.java
@@ -0,0 +1,13 @@
+package com.red5pro.awsdeployer;
+
+/**
+ * Hello world!
+ *
+ */
+public class AutoDeployer 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
