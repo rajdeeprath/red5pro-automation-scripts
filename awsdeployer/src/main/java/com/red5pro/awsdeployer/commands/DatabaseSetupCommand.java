ec32ac05acdf6c77c2b681364d63c93e27795a58
rajdeeprath <rajdeeprath@gmail.com>
2018-01-21 06:13:01 +0530

    Database instance creation
    + Configuration update
    + Creating database  security group
    + Creating database instance

--- awsdeployer/src/main/java/com/red5pro/awsdeployer/commands/DatabaseSetupCommand.java
+++ awsdeployer/src/main/java/com/red5pro/awsdeployer/commands/DatabaseSetupCommand.java
@@ -1,9 +1,40 @@
 package com.red5pro.awsdeployer.commands;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.apache.commons.chain.Command;
 import org.apache.commons.chain.Context;
 
+import com.amazonaws.auth.AWSCredentials;
+import com.amazonaws.auth.AWSStaticCredentialsProvider;
+import com.amazonaws.services.ec2.AmazonEC2;
+import com.amazonaws.services.ec2.AmazonEC2ClientBuilder;
+import com.amazonaws.services.ec2.model.CreateSecurityGroupResult;
+import com.amazonaws.services.rds.AmazonRDS;
+import com.amazonaws.services.rds.AmazonRDSClient;
+import com.amazonaws.services.rds.AmazonRDSClientBuilder;
+import com.amazonaws.services.rds.model.CreateDBInstanceRequest;
+import com.amazonaws.services.rds.model.CreateDBSecurityGroupRequest;
+import com.amazonaws.services.rds.model.DBInstance;
+import com.amazonaws.services.rds.model.DBSecurityGroup;
+import com.amazonaws.services.rds.model.DescribeDBInstancesRequest;
+import com.amazonaws.services.rds.model.DescribeDBInstancesResult;
+import com.amazonaws.services.rds.model.DescribeDBSecurityGroupsRequest;
+import com.amazonaws.services.rds.model.DescribeDBSecurityGroupsResult;
+import com.amazonaws.services.rds.model.Filter;
+import com.amazonaws.services.rds.model.Tag;
+import com.red5pro.awsdeployer.model.Configuration;
+
 public class DatabaseSetupCommand implements Command {
+	
+	private Configuration configuration;
+	
+	private AWSCredentials credentials;
+	
+	private DBSecurityGroup securityGroup;
+	
+	private DBInstance instance;
 	
 	
 	public DatabaseSetupCommand()
@@ -15,7 +46,114 @@ public class DatabaseSetupCommand implements Command {
 	@Override
 	public boolean execute(Context context) throws Exception 
 	{
+		System.out.println("Reserving IP address");
+		
+		this.configuration = (Configuration) context.get("configuration");		
+		this.credentials = (AWSCredentials) context.get("credentials");
+		
+		AmazonRDS client = getRDSClient(configuration.getDbRegion());
+		
+		Filter sessionIdFilter = new Filter().withName("tag:sessionid").withValues(configuration.getSessionId());
+		
+		DescribeDBSecurityGroupsRequest dbSecurityGroupRequest =  new DescribeDBSecurityGroupsRequest().withFilters(new Filter[]{sessionIdFilter}).withDBSecurityGroupName(configuration.getDbSecurityGroupName());
+		DescribeDBSecurityGroupsResult dbSecurityGroupResult =  client.describeDBSecurityGroups(dbSecurityGroupRequest);
+		List<DBSecurityGroup> securityGroups = dbSecurityGroupResult.getDBSecurityGroups();
+		
+		
+		if(securityGroups.size() > 1)
+		{
+			System.err.print("An unexpected erro occurred. There seems to be two identical security groups!! I dont know what to do next... ");
+			throw new Exception("Duplicate security groups detected for database!!!");
+		}
+		else if(securityGroups.size() == 1)
+		{
+			securityGroup = securityGroups.get(0);
+			System.out.print("DB Security group found by name " + securityGroup.getDBSecurityGroupName() + " in region " + configuration.getDbRegion());
+		}
+		else
+		{
+			List<Tag> dbGroupTags = new ArrayList<Tag>();
+			Tag sessionId = new Tag();
+			sessionId.setKey("sessionId");
+			sessionId.setValue(configuration.getSessionId());
+			dbGroupTags.add(sessionId);			
+			
+			CreateDBSecurityGroupRequest createSecurityGroupRequest = new CreateDBSecurityGroupRequest();
+			createSecurityGroupRequest.withDBSecurityGroupName(configuration.getDbSecurityGroupName()).withTags(dbGroupTags);
+			securityGroup=  client.createDBSecurityGroup(createSecurityGroupRequest);
+			
+			System.out.print("DB Security group created by name " + securityGroup.getDBSecurityGroupName() + " in region " + configuration.getDbRegion());
+		}
+		
+		
+		// Check and create db
+		
+		DescribeDBInstancesRequest checkDbReq = new DescribeDBInstancesRequest().withFilters(new Filter[] {sessionIdFilter}).withDBInstanceIdentifier(configuration.getDbInstanceName());
+		DescribeDBInstancesResult dbResult =  client.describeDBInstances(checkDbReq);
+		List<DBInstance> instances = dbResult.getDBInstances();
+		
+		if(instances.size() == 0)
+		{
+			List<Tag> dbInstanceTags = new ArrayList<Tag>();
+			Tag sessionId = new Tag();
+			sessionId.setKey("sessionId");
+			sessionId.setValue(configuration.getSessionId());
+			dbInstanceTags.add(sessionId);			
+			
+			CreateDBInstanceRequest createDBInstanceRequest = new CreateDBInstanceRequest() //
+	         .withDBInstanceIdentifier(configuration.getDbInstanceName()) //
+	         .withDBName(configuration.getDbSchemaName()) //
+	         .withEngine("MySQL") //
+	         .withEngineVersion(configuration.getDbEngineVersion()) //
+	         .withDBInstanceClass(configuration.getDbInstanceSize()) //
+	         .withMasterUsername(configuration.getDbUsername()) //
+	         .withMasterUserPassword(configuration.getDbPassword()) //
+	         .withAllocatedStorage(configuration.getDbAllocationSize()) //
+	         .withBackupRetentionPeriod(0) //
+	         .withMultiAZ(false)
+	         .withDBSecurityGroups(configuration.getDbSecurityGroupName()) //
+	         .withLicenseModel("general-public-license") 
+	         .withTags(dbInstanceTags);
+			
+			instance = client.createDBInstance(createDBInstanceRequest);
+		}
+		else
+		{
+			instance = instances.get(0);
+		}
+		
+		
+		 // Check for db to be in running state
+		 
+		 
+		 // connect to db and populate the tables if not exists
+		 
+		 
+		 // finish
+
 		return false;
+	}
+
+	
+	
+	AmazonEC2 getEc2Client2(String region){
+		AmazonEC2 ec2 = AmazonEC2ClientBuilder.standard()
+                .withRegion(region)
+                .withCredentials(new AWSStaticCredentialsProvider(credentials))
+                .build();
+		
+		return ec2;
+	}
+
+	
+	
+	AmazonRDS getRDSClient(String region){
+		AmazonRDS rds = AmazonRDSClientBuilder.standard()
+                .withRegion(region)
+                .withCredentials(new AWSStaticCredentialsProvider(credentials))
+                .build();
+		
+		return rds;
 	}
 
 }
--- awsdeployer/src/main/java/com/red5pro/awsdeployer/model/Configuration.java
+++ awsdeployer/src/main/java/com/red5pro/awsdeployer/model/Configuration.java
@@ -16,12 +16,29 @@ public class Configuration {
 	
 	private String[] streamManagerRegions = {"useast-1"};
 	
-	private String sessionId = "red5proautoscaling"; // must be unique
+	private String sessionId = "red5proautoscaling";
 	
-	private String vpcName = "autoscalignvpc";
+	private String vpcName = "infrared5vpc";
 	
 	private String vpcCidr = "10.0.0.0/16";
+	
+	private String dbInstanceName = "streammanager-db";
+	
+	private String dbSchemaName = "cluster";
+	
+	private String dbUsername = "ir5user";
+	
+	private String dbPassword = "xyz@1234567";
+	
+	private String dbRegion = "useast-1";
+	
+	private String dbInstanceSize = "db.m1.small";
+	
+	private String dbEngineVersion = "5.6.37";
 
+	private int dbAllocationSize = 20;
+	
+	private String dbSecurityGroupName = "autoscalingrds";
 	
 
 	public String getAwsAccessSecret() {
@@ -102,6 +119,78 @@ public class Configuration {
 
 	public void setVpcCidr(String vpcCidr) {
 		this.vpcCidr = vpcCidr;
+	}
+
+	public String getDbSchemaName() {
+		return dbSchemaName;
+	}
+
+	public void setDbSchemaName(String dbSchemaName) {
+		this.dbSchemaName = dbSchemaName;
+	}
+
+	public String getDbPassword() {
+		return dbPassword;
+	}
+
+	public void setDbPassword(String dbPassword) {
+		this.dbPassword = dbPassword;
+	}
+
+	public String getDbUsername() {
+		return dbUsername;
+	}
+
+	public void setDbUsername(String dbUsername) {
+		this.dbUsername = dbUsername;
+	}
+
+	public String getDbInstanceName() {
+		return dbInstanceName;
+	}
+
+	public void setDbInstanceName(String dbInstanceName) {
+		this.dbInstanceName = dbInstanceName;
+	}
+
+	public String getDbRegion() {
+		return dbRegion;
+	}
+
+	public void setDbRegion(String dbRegion) {
+		this.dbRegion = dbRegion;
+	}
+
+	public String getDbInstanceSize() {
+		return dbInstanceSize;
+	}
+
+	public void setDbInstanceSize(String dbInstanceSize) {
+		this.dbInstanceSize = dbInstanceSize;
+	}
+
+	public String getDbEngineVersion() {
+		return dbEngineVersion;
+	}
+
+	public void setDbEngineVersion(String dbInstanceVersion) {
+		this.dbEngineVersion = dbInstanceVersion;
+	}
+
+	public int getDbAllocationSize() {
+		return dbAllocationSize;
+	}
+
+	public void setDbAllocationSize(int dbAllocationSize) {
+		this.dbAllocationSize = dbAllocationSize;
+	}
+
+	public String getDbSecurityGroupName() {
+		return dbSecurityGroupName;
+	}
+
+	public void setDbSecurityGroupName(String dbSecurityGroupName) {
+		this.dbSecurityGroupName = dbSecurityGroupName;
 	}
 	
 	
